1da02f39e93adaa8a7e193ca6e251a4c9d7e6225
SoYoungLEE-me <lsy000626@gmail.com>
2026-02-05 07:39:15 +0900

    feat: Todo API 기본 CRUD 구현

---
+++ controller/task.controller.js
@@ -0,0 +1,60 @@
+const Task = require("../model/Task");
+
+const taskController = {};
+
+taskController.createTask = async (req, res) => {
+  try {
+    const { task, isComplete } = req.body;
+    const newTask = new Task({ task, isComplete });
+    await newTask.save().select("-__v");
+    res.status(200).json({ status: "ok", data: newTask });
+  } catch (err) {
+    res.status(400).json({ status: "fail", error: err });
+  }
+};
+
+taskController.getTask = async (req, res) => {
+  try {
+    const taskList = await Task.find({});
+    res.status(200).json({ status: "ok", data: taskList });
+  } catch (err) {
+    res.status(400).json({ status: "fail", error: err });
+  }
+};
+
+taskController.updateTask = async (req, res) => {
+  try {
+    const { task, isComplete } = req.body;
+    const { id } = req.params;
+    const updatedTask = await Task.findByIdAndUpdate(
+      id,
+      { task, isComplete },
+      { new: true, runValidators: true }
+    ).select("-__v");
+
+    if (!updatedTask) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+
+    res.status(200).json({ status: "ok", data: updatedTask });
+  } catch (err) {
+    res.status(400).json({ status: "fail", error: err });
+  }
+};
+
+taskController.deleteTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedTask = await Task.findByIdAndDelete(id).select("-__v");
+
+    if (!deletedTask) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+
+    res.status(200).json({ status: "ok", data: deletedTask });
+  } catch (err) {
+    res.status(400).json({ status: "fail", error: err });
+  }
+};
+
+module.exports = taskController;
